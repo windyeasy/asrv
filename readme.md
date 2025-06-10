13913dbfe221a4a7906d6e793b7f09412a866d17
windyeasy <windyeasy13gb@163.com>
2025-06-10 21:32:13 +0800

    chore: init

---
+++ readme.md
@@ -0,0 +1,11 @@
+# assist-server
+
+一个开发助手服务
+
+## TODO
+
+
+
+## License
+
+[MIT](./LICENSE) License © [windyeasy](https://github.com/windyeasy)
---
+++ rollup.config.js
@@ -0,0 +1,53 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+
+function createConfig(isProperty) {
+  const sourcemap = isProperty ? false : true
+  /**
+   * @type {import('rollup').RollupOptions}
+   */
+  return [
+    {
+      input: 'src/index.ts',
+      output: [
+        {
+          file: './dist/index.cjs',
+          format: 'cjs',
+          exports: 'named',
+          sourcemap,
+        },
+        {
+          file: './dist/index.mjs',
+          format: 'es',
+          sourcemap,
+        },
+      ],
+      plugins: [
+        resolve({
+          extensions: ['.js', '.ts', '.json', '.tsx', '.jsx', '.mjs', '.mts', '.cjs'],
+        }),
+        commonjs(),
+        typescript({
+          tsconfig: './tsconfig.json',
+          sourceMap: sourcemap,
+        }),
+      ],
+    },
+    {
+      input: 'src/index.ts',
+      output: {
+        file: 'dist/index.d.ts',
+        format: 'es',
+      },
+      plugins: [dts()],
+    },
+  ]
+}
+
+export default (commandLineArguments) => {
+  const isDevelopment = commandLineArguments.watch
+
+  return createConfig(!isDevelopment)
+}
