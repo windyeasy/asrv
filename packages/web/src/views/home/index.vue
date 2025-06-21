bfe3629b1b81fbfbac216c25fe4055eb5390b308
windyeasy <windyeasy13gb@163.com>
2025-06-21 19:31:29 +0800

    feat: 更新页面

--- packages/web/src/views/home/index.vue
+++ packages/web/src/views/home/index.vue
@@ -6,11 +6,54 @@
     <div class="description text-size-lg ml-2em">
       A server-side tool that assists front-end development.
     </div>
+    <main class="mt-10">
+      <section class="flex py-2">
+        <div class="title mr-2">
+          API Swagger:
+        </div>
+        <div class="link">
+          <a
+            rel="noreferrer"
+            href="https://api.juejin.cn/swagger-ui/index.html"
+            target="_blank"
+            class=" text-green"
+          >
+            https://api.juejin.cn/swagger-ui/index.html
+          </a>
+        </div>
+      </section>
+      <section class="flex py-2">
+        <div class="title mr-2">
+          History:
+        </div>
+        <div class="link">
+          <a
+            rel="noreferrer"
+            href="https://api.juejin.cn/swagger-ui/index.html"
+            target="_blank"
+            class="text-green"
+          >
+            https://api.juejin.cn/swagger-ui/index.html
+          </a>
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .description {
   color: var(--text-sub-color);
+}
+main {
+  font-size: 14px;
+
+  .title {
+    color: var(--text-sub-color);
+  }
+  .link a {
+    text-decoration: none;
+
+  }
 }
 </style>
