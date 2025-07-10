09e870ecd3cc7fa2b785bfdd4de33ec30a2ed436
windyeasy <windyeasy13gb@163.com>
2025-07-10 21:39:42 +0800

    fix: vitepress add base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: 'asrv',
+  base: '/asrv/',
   description: 'A server-side tool that assists front-end development',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
